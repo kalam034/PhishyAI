f23044ecfb5519bf03c44946d26fc38fc1ab0e3b
Khalid Alam <kalam034@uottawa.ca>
2020-02-10 00:31:12 -0500

    feature extraction implemented

--- classifier.py
+++ classifier.py
@@ -1,15 +1,29 @@
 import pandas as pd
 
-import merge_dataframes
+from feature_extractor import feature_extractor
 
-training_df = pd.read_csv("training_datasets/merged_dataframes.csv", header = 0)
 
-bn_records = training_df.loc[training_df ['Label'] == 0]
-ml_records = training_df.loc[training_df ['Label'] == 1]
+def print_number_records(dataframe):
+    
+    bn_records = dataframe.loc[dataframe ['Label'] == 0]
+    ml_records = dataframe.loc[dataframe ['Label'] == 1]
 
-print("Total number of records in training dataframe: " + str(training_df.shape[0]))
-print("Total number of benign records in training dataframe: + " + str(bn_records.shape[0]))
-print("Total number of malicious records in training dataframe: + " + str(ml_records.shape[0]))
+    print("Total number of records in training dataframe: " + str(dataframe.shape[0]))
+    print("Total number of benign records in training dataframe: + " + str(bn_records.shape[0]))
+    print("Total number of malicious records in training dataframe: + " + str(ml_records.shape[0]))
+
+
+if __name__ == "__main__":
+
+    df = pd.read_csv("training_datasets/merged_dataframes_100.csv", header = 0)
+
+    ft = feature_extractor(df)
+    df_features = ft.extract_features()
+    df_features.to_csv("training_datasets/dataframe_with_features.csv")
+    
+
+
+
 
 
     
