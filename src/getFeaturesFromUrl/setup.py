b9641c937caae88153186246f746c880e1270bf1
Matt Orchard <morch028@uottawa.ca>
2020-03-21 13:16:07 -0400

    Adds script for creating cloud function deployment artifact

---
+++ src/getFeaturesFromUrl/setup.py
@@ -0,0 +1,26 @@
+from zipfile import ZipFile
+
+local_dependencies = [
+    "requirements.txt",
+    "main.py"
+]
+aliased_dependencies = {
+    "../features/build_features.py": "features/build_features.py",
+    "../features/url.py": "features/url.py",
+    "../features/url_utils.py": "features/url_utils.py"
+}
+
+
+if __name__ == "__main__":
+    print("Creating ZIP for feature extraction endpoint")
+
+    with ZipFile("getFeaturesFromUrl.zip", "w") as dist:
+        for file in local_dependencies:
+            dist.write(file)
+            print("Added [%s] to zip" % file)
+
+        for real_file, arc_file in aliased_dependencies.items():
+            dist.write(real_file, arcname=arc_file)
+            print("Added [%s] to zip" % real_file)
+
+    print("Created ZIP for feature extraction endpoint successfully")
