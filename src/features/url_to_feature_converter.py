2a4e1d7329bb94c35f7f64ba6bfd5940a4f15b65
Khalid Alam <kalam034@uottawa.ca>
2020-03-21 12:18:35 -0400

    build not bulid

---
+++ src/features/url_to_feature_converter.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+from . import build_features as ft
+
+
+def url_to_features(url):
+    data = {'Url': [url]}
+
+    dataframe = pd.read_json(data)
+
+    dataframe = ft.extract_features(dataframe)
+
+    return dataframe.to_numpy()
